2bde9f1837fc9dd21797baa7494a1b5f31eb7936
DevExpressExampleBot <fakeEmail>
2023-01-27 12:41:10 +0400

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,57 +1,56 @@
-﻿Imports System
 Imports System.Drawing
 Imports System.Windows
-
-#Region "#usings"
-Imports DevExpress.XtraRichEdit
+'#Region "#usings"
 Imports DevExpress.XtraRichEdit.API.Native
 Imports DevExpress.XtraRichEdit.Commands
-#End Region ' #usings
 
+'#End Region  ' #usings
 Namespace Table_API_Example
+
     ''' <summary>
     ''' Interaction logic for MainWindow.xaml
     ''' </summary>
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
 
-        #Region "#applystyles"
+'#Region "#applystyles"
         Private Shared Sub ApplyStyles(ByVal doc As Document, ByVal _table As Table, ByVal header_Row As TableRow)
             _table.Style = doc.TableStyles("MyTableStyle")
             For Each p As Paragraph In doc.Paragraphs.Get(header_Row.Range)
                 p.Style = doc.ParagraphStyles("MyTableHeaderStyle")
-            Next p
+            Next
+
             For Each tr As TableRow In _table.Rows
                 If Not tr.IsFirst Then
                     tr.FirstCell.BackgroundColor = Color.Empty
                     doc.Paragraphs.Get(tr.FirstCell.Range.Start).Style = doc.ParagraphStyles("MyTableCaptionStyle")
                 End If
-            Next tr
+            Next
         End Sub
-        #End Region ' #applystyles
-        #Region "#applyborders"
+
+'#End Region  ' #applystyles
+'#Region "#applyborders"
         Private Shared Sub ApplyBorders(ByVal _table As Table)
             _table.Borders.InsideHorizontalBorder.LineThickness = 1
             _table.Borders.InsideHorizontalBorder.LineStyle = TableBorderLineStyle.Double
             _table.Borders.InsideVerticalBorder.LineThickness = 1
             _table.Borders.InsideVerticalBorder.LineStyle = TableBorderLineStyle.Double
-
             For Each _cell As TableCell In _table.Rows.First.Cells
                 _cell.BackgroundColor = Color.Empty
                 _cell.Borders.Bottom.LineStyle = TableBorderLineStyle.None
                 _cell.Borders.Top.LineStyle = TableBorderLineStyle.None
                 _cell.Borders.Left.LineStyle = TableBorderLineStyle.None
                 _cell.Borders.Right.LineStyle = TableBorderLineStyle.None
-            Next _cell
+            Next
         End Sub
-        #End Region ' #applyborders
 
+'#End Region  ' #applyborders
         Private Sub btnInsertTable_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            Dim doc As Document = richEditControl1.Document
+            Dim doc As Document = Me.richEditControl1.Document
             CreateTableStyles(doc)
             ' Insert a table
             Dim _table As Table = doc.Tables.Create(doc.Selection.Start, 8, 8, AutoFitBehaviorType.AutoFitToWindow)
@@ -61,7 +60,7 @@ Namespace Table_API_Example
             IterateCells(_table)
             ' Insert a column to the left for row numbering 
             doc.Selection = _table.Rows(1).FirstCell.Range
-            Dim cmd As New InsertTableColumnToTheLeftCommand(richEditControl1)
+            Dim cmd As InsertTableColumnToTheLeftCommand = New InsertTableColumnToTheLeftCommand(Me.richEditControl1)
             cmd.Execute()
             _table.ForEachRow(New TableRowProcessorDelegate(AddressOf InsertRowNumber))
             ' Insert a row for column captions
@@ -70,12 +69,13 @@ Namespace Table_API_Example
             ' Insert a header row
             Dim header_Row As TableRow = InsertHeader(_table, "Multiplication Table")
             ' Finalize formatting
-            ApplyBorders(_table)
-            ApplyStyles(doc, _table, header_Row)
+            Call ApplyBorders(_table)
+            Call ApplyStyles(doc, _table, header_Row)
             ' End table modification
             _table.EndUpdate()
         End Sub
-        #Region "#createtablestyles"
+
+'#Region "#createtablestyles"
         Private Sub CreateTableStyles(ByVal doc As Document)
             Dim tStyleMain As TableStyle = doc.TableStyles.CreateNew()
             tStyleMain.AllCaps = True
@@ -86,7 +86,6 @@ Namespace Table_API_Example
             Dim tableStyleCellProperties As TableCellPropertiesBase = CType(tStyleMain, TableCellPropertiesBase)
             tableStyleCellProperties.CellBackgroundColor = Color.Yellow
             doc.TableStyles.Add(tStyleMain)
-
             Dim style_Header As ParagraphStyle = doc.ParagraphStyles.CreateNew()
             style_Header.FontName = "Comic Sans"
             style_Header.FontSize = 18
@@ -94,7 +93,6 @@ Namespace Table_API_Example
             style_Header.Bold = True
             style_Header.Name = "MyTableHeaderStyle"
             doc.ParagraphStyles.Add(style_Header)
-
             Dim style_Caption As ParagraphStyle = doc.ParagraphStyles.CreateNew()
             style_Caption.FontName = "Comic Sans"
             style_Caption.FontSize = 14
@@ -103,23 +101,24 @@ Namespace Table_API_Example
             style_Caption.Name = "MyTableCaptionStyle"
             doc.ParagraphStyles.Add(style_Caption)
         End Sub
-        #End Region ' #createtablestyles
-        #Region "#celldelegate"
+
+'#End Region  ' #createtablestyles
+'#Region "#celldelegate"
         Private Sub IterateCells(ByVal _table As Table)
             _table.BeginUpdate()
             _table.ForEachCell(New TableCellProcessorDelegate(AddressOf MakeMultiplicationCell))
             _table.EndUpdate()
         End Sub
+
         Private Sub MakeMultiplicationCell(ByVal cell As TableCell, ByVal i As Integer, ByVal j As Integer)
-            richEditControl1.Document.InsertText((cell).Range.Start, String.Format("{0}*{1} is {2}", i + 2, j + 2, (i + 2) * (j + 2)))
+            Me.richEditControl1.Document.InsertText(cell.Range.Start, String.Format("{0}*{1} is {2}", i + 2, j + 2, (i + 2) * (j + 2)))
         End Sub
-        #End Region ' #celldelegate
-        #Region "#insertcolumnnumbers"
+
+'#End Region  ' #celldelegate
+'#Region "#insertcolumnnumbers"
         Private Sub InsertColumnNumbers(ByVal row As TableRow)
             For Each _cell As TableCell In row.Cells
-                If _cell.Index = 0 Then
-                    Continue For
-                End If
+                If _cell.Index = 0 Then Continue For
                 _cell.BackgroundColor = Color.Empty
                 Dim doc As SubDocument = _cell.Range.BeginUpdateDocument()
                 Dim range As DocumentRange = doc.InsertSingleLineText(_cell.Range.Start, String.Format("{0}", _cell.Index + 1))
@@ -130,10 +129,11 @@ Namespace Table_API_Example
                 cp.ForeColor = Color.Red
                 doc.EndUpdateCharacters(cp)
                 range.EndUpdateDocument(doc)
-            Next _cell
+            Next
         End Sub
-        #End Region ' #insertcolumnnumbers
-        #Region "#insertheader"
+
+'#End Region  ' #insertcolumnnumbers
+'#Region "#insertheader"
         Private Function InsertHeader(ByVal _table As Table, ByVal caption As String) As TableRow
             _table.BeginUpdate()
             Dim row As TableRow = _table.Rows.InsertBefore(0)
@@ -144,14 +144,14 @@ Namespace Table_API_Example
             _table.EndUpdate()
             Return row
         End Function
-        #End Region ' #insertheader
-        #Region "#insertrownumber"
+
+'#End Region  ' #insertheader
+'#Region "#insertrownumber"
         Private Sub InsertRowNumber(ByVal row As TableRow, ByVal rowNumber As Integer)
             Dim doc As SubDocument = row.FirstCell.Range.BeginUpdateDocument()
             Dim range As DocumentRange = doc.InsertText(row.FirstCell.Range.Start, String.Format("{0}", rowNumber + 2))
             range.EndUpdateDocument(doc)
         End Sub
-        #End Region ' #insertrownumber
-
+'#End Region  ' #insertrownumber
     End Class
 End Namespace
